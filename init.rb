1bfe074d9e6f474610dd72ff4def0566ce7853c8
kokoriru <kokorirury@yahoo.co.jp>
2016-03-13 23:33:31 +0900

    コアのモデルを拡張して関連を設定

--- init.rb
+++ init.rb
@@ -1,3 +1,7 @@
+Dir[File.expand_path('../lib/show_important_projects', __FILE__) << '/*.rb'].each do |file|
+  require_dependency file
+end
+
 Redmine::Plugin.register :show_important_projects do
   name 'Show Important Projects plugin'
   author 'Author name'
